efd32fc1b6e0c971f58cf9aefd677cb3c161f1e0
root <liammaeke909@gmail.com>
2023-09-18 03:17:09 -0700

    1

---
+++ _printf.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - Printf function
+ * @format: The format string
+ *
+ * Return: The number of printed characters
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			count++;
+		}
+		else
+		{
+			format++;
+			if (*format == '/0')
+				return (-1);
+			if (*format == 'c')
+			{
+				char c = va_args(args, int);
+
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_args(args, char *);
+				if (s == NULL)
+					s = "(null)";
+				while (*s)
+				{
+					write(1, s, 1);
+					s++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else
+				return (-1);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN.H
+#define MAIN.H
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+
+#endif
