a45144d17357dc2358e5e88d2d10db4ba4e6cba6
liammkl <liammaeke909@gmail.com>
2023-09-18 05:33:59 -0700

    2

--- _printf_specifiers.c
+++ _printf_specifiers.c
@@ -1,56 +1,87 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * _printf - Custom printf function
- * @format: The format string
- * Return: Number of characters printed
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
-
+	int printed_chars = 0;
+	char *str;
+	
 	va_start(args, format);
-
+	
 	while (*format)
 	{
-		if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
+		if (*format == '%')
 		{
-			int num = va_arg(args, int);
-			char buffer[12];
-			int len = 0;
-
-			if (num < 0)
+			format++; /* Move past '%' */
+			if (*format == '\0') /* If '%' is at the end, break */
+				break;
+			
+			if (*format == 'c')
 			{
-				write(1, "-", 1);
-				num = -num;
-				count++;
+				int c = va_arg(args, int);
+				printed_chars += write(1, &c, 1);
 			}
-
-			while (num > 0)
+			else if (*format == 's')
 			{
-				buffer[len++] = '0' + (num % 10);
-				num /= 10;
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					printed_chars += write(1, str, 1);
+					str++;
+				}
 			}
-
-			while (len > 0)
+			
+			else if (*format == 'd' || *format == 'i')
 			{
-				write(1, &buffer[--len], 1);
-				count++;
+				int num = va_arg(args, int);
+				printed_chars += _print_number(num);
 			}
-
-			format += 2;
+			else if (*format == '%')
+			{
+				printed_chars += write(1, "%", 1);
+			}
+			else
+			{
+				printed_chars += write(1, "%", 1);
+				printed_chars += write(1, format, 1);
+			}
 		}
 		else
 		{
-			write(1, format, 1);
-			count++;
-			format++;
+			printed_chars += write(1, format, 1);
 		}
+		format++;
 	}
-
 	va_end(args);
-	return count;
+	return (printed_chars);
+}
+
+/**
+ * _print_number - Prints an integer.
+ * @n: The integer to print.
+ *
+ * Return: The number of characters printed.
+ */
+int _print_number(int n)
+{
+	int printed_chars = 0;
+	if (n < 0)
+	{
+		printed_chars += write(1, "-", 1);
+		n = -n;
+	}
+	if (n / 10)
+		printed_chars += _print_number(n / 10);
+	char digit = (n % 10) + '0';
+	printed_chars += write(1, &digit, 1);
+	return (printed_chars);
 }
